38102c62dc8a16dcafe021b14086b386f29f0b5d
alessiotinaglia <alessio.tina1998@gmail.com>
2024-12-03 16:51:24 +0100

    fine primo bonus

--- dati/array.js
+++ dati/array.js
@@ -1,29 +1,34 @@
 const posts = [
     {
+        "id": 1, 
         titolo: "Ricetta ciambellone",
         contenuto: "Oggi parleremo della ricetta del ciambellone",
         immagine: ('<img scr="/images/ciambellone.jpeg">'),
         tags: ["Uova", "Farina", "Burro"]
     },
     {
+        "id": 2,
         titolo: "Ricetta cracker alla barbabietola",
         contenuto: "Oggi parleremo delle ricetta cracker alla barbabietola",
         immagine: ('<img scr="/images/cracker_barbabietola.jpeg">'),
         tags: ["Cracker", "Barbabietola", "Olio"]
     },
     {
+        "id": 3,
         titolo: "Ricetta pane fritto",
         contenuto: "Oggi parleremo della ricetta del pane fritto",
         immagine: ('<img scr="/images/pane_fritto_dolce.jpeg">'),
         tags: ["Pane", "latte", "uova"]
     },
     {
+        "id": 4,
         titolo: "Ricetta barbabietola",
         contenuto: "Oggi parleremo della ricetta barbabietola",
         immagine: ('<img scr="/images/pasta_barbabietola.jpeg">'),
         tags: ["Sale", "Colorante", "Latte"]
     },
     {
+        "id": 5,
         titolo: "Ricetta torta_paesana",
         contenuto: "Oggi parleremo della ricetta torta_paesana",
         immagine: ('<img scr="/images/torta_paesana.jpeg">'),
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 
 // Importa il router da posts.js
 const postsRouter = require("./router/posts.js");
+const posts = require("./dati/array.js");
 
 // immagini statiche
 app.use(express.static("public"));
@@ -15,13 +16,21 @@ app.use(express.static("public"));
 // Usa il router per gestire le rotte /posts
 app.use("/posts", postsRouter);
 
-// Rotta principale per servire index.html dalla cartella pages
+// Rotta principale
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/router/posts.js");
-  res.send("<h1>Server del mio blog</h1>");
+    res.sendFile(__dirname + "/router/posts.js");
+    res.send("<h1>Server del mio blog</h1>");
+});
+
+app.get("/bacheca", (req, res) => {
+    const result = {
+        conteggio: posts.length, 
+        posts: posts             
+    };
+    res.json(result); 
 });
 
 // In ascolto sulla porta 3000
 app.listen(PORT, () => {
-  console.log(`Server in esecuzione su http://localhost:${PORT}`);
+    console.log(`Server in esecuzione su http://localhost:${PORT}`);
 });
