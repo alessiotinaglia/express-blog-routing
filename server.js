edffbef3420b5b6e657c568d516cff1d0a572c9a
alessiotinaglia <alessio.tina1998@gmail.com>
2024-12-03 16:30:53 +0100

    collegamento due file

--- server.js
+++ server.js
@@ -3,19 +3,22 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// Middleware per gestire il body JSON
+app.use(express.json());
+
 // Importa il router da posts.js
-const postsRouter = require("./routers/posts.js");
+const postsRouter = require("./router/posts.js");
 
-// immagini statiche 
-app.use(express.static('public'));
+// immagini statiche
+app.use(express.static("public"));
 
-
-// Usa il router per gestire le rotte /pets
-app.use("/pets", postsRouter);  
+// Usa il router per gestire le rotte /posts
+app.use("/posts", postsRouter);
 
 // Rotta principale per servire index.html dalla cartella pages
 app.get("/", (req, res) => {
-  res.sendFile({ root: __dirname + "/pages" });
+  res.sendFile(__dirname + "/router/posts.js");
+  res.send("<h1>Server del mio blog</h1>");
 });
 
 // In ascolto sulla porta 3000
